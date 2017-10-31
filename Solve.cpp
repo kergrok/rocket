143334cfaab4766cae33f570fc4fe149a59ab9e8
GOUIN Camille <camgouin@sidobre.pedago.ipb.fr>
2017-10-31 14:51:49 +0100

    Fin de Solve.cpp, maj Type.h et création makefile

--- Solve.cpp
+++ Solve.cpp
@@ -7,19 +7,33 @@ using namespace std;
 using namespace Eigen;
 
 /* A terminer */
-void Calc_density(int num_maille)
+void Mesh::Calc_prop(int i)
 {
   double surface = _maille[i].Getsurf();
-  Eigen::VectorXi part = _maille[i].Getindices();
+  VectorXi part = _maille[i].Getindices();
   int nb_part = part.size();
 
   double density = nb_part * _omega / surface;
+  _maille[i].Modifydensity(density);
 
-  Eigen::Vector3d velocity = _mpoint[part[0]].Getvelo();
-  double velocity_moy = 0;
-  for (int i = 0; i < nb_part; i++) {
-    velocity = _mpoint[part[i]].Getvelo();
-
+  Vector3d velocity_moy;
+  velocity_moy.setZero();
+  for (int j = 0; j < nb_part; j++)
+  {
+    velocity_moy += _part[j].Getvelo();
+    velocity_moy = _omega*velocity_moy/surface;
   }
+  _maille[i].Modifyvelo(velocity_moy);
+  double temp = 0;
+  double R = 8.314;
+  for (int j = 0; j < nb_part; j++)
+  {
+    temp += pow(_part[j].Getvelo()[0]-_maille[i].Getaverage()[0],2);
+    temp += pow(_part[j].Getvelo()[1]-_maille[i].Getaverage()[1],2);
+    temp += pow(_part[j].Getvelo()[2]-_maille[i].Getaverage()[2],2);
+  }
+  temp = (_omega/3)*temp/(surface*R);
+  _maille[i].Modifytemp(temp);
+
 
 }
