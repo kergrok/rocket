4006319ae7d4023e95c7bea0277e4f51c49f2d5a
ANTIMES Aymeric <aantimes@judygarland.pedago.ipb.fr>
2017-11-06 14:48:12 +0100

    Generateur loi normale variance sigma, centrée median

--- alea.cpp
+++ alea.cpp
@@ -6,30 +6,14 @@
 using namespace std;
 
 
-int main()
+double alea(double sigma, double median )
 {
-
-  int nrolls=150000;
-  double RT=8.314*200;
-  double deuxpi=3.14159265358979*2;
-
-  double sigma=RT;
-  double median=0;
-
 
   random_device generator;
   normal_distribution<double> distribution(median,sigma);
-
-  int p[10]={};
-  double sum=0;
-  for (int i=0; i<nrolls; ++i) {
-    double number = distribution(generator);/* Nombre généré suivant loi normale (median,sigma)*/
-    sum=sum+number*number;
-  }
-
-  sum=sum/nrolls/RT/RT;
-
-  cout<<"sum="<<sum<<endl;/* Vérification que la loi est bien normale: Doit etre proche de 1 pour nrolls grand*/
-
-  return 0;
+  
+  double number = distribution(generator);/* Nombre généré suivant loi normale (median,sigma)*/
+   
+  return number
+  
 }
