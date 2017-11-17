7740d9788a83c1de7c4b8e17227ebfc269b71b6a
bouchex <bouchex.sylvain@gmail.com>
2017-11-17 17:28:45 +0100

    Création de la boucle en tps

--- Computing.cpp
+++ Computing.cpp
@@ -8,11 +8,24 @@ using namespace Eigen;
 
 void Mesh::initialize()
 {
-  Buildvoisins();
   ReadParameter();
+  Buildvoisins();
+  CFL();
+  Create_in_Flow();
 }
 
 void Mesh::compute()
 {
-
+  double t;
+  t=0.;
+  while (t<_Temps_final)
+  {
+    Displacement();
+    do (int i=0; i<_maille.size();i++)
+    {
+      Calc_prop(i);
+    }
+    Create_in_Flow(); 
+    t+=_dt;
+  }
 }
