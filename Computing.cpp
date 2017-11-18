0177d66f21ff691b5a97fcf91499d26d57f10d9a
ANTIMES Aymeric <aantimes@lakijigar.pedago.ipb.fr>
2017-11-18 14:51:26 +0100

    Remise de computing

--- Computing.cpp
+++ Computing.cpp
@@ -21,7 +21,7 @@ void Mesh::compute()
   while (t<_Temps_final)
   {
     Displacement();
-    do (int i=0; i<_maille.size();i++)
+    for (int i=0; i<_maille.size();i++)
     {
       Calc_prop(i);
     }
