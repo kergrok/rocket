86dbc26bef3898508770ca9d847d26108b1842f8
BOUET Lola <lbouet@akranes.pedago.ipb.fr>
2017-11-19 16:22:20 +0100

    Correction de l'écriture

--- Computing.cpp
+++ Computing.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "Mesh.h"
 #include "Dense"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 using namespace Eigen;
@@ -18,15 +20,21 @@ void Mesh::compute()
 {
   double t;
   t=0.;
+
+  cout << _Temps_final << endl;
+  cout << _dt << endl;
+  int k = 0;
+
   while (t<_Temps_final)
   {
+    k++;
     Displacement();
     MajMailleParticule();
     for (int i=0; i<_maille.size();i++)
     {
       Calc_prop(i);
-      write("solDens"+to_string(i)+".inp");
     }
+    write("solDens"+to_string(k)+".inp");
     Create_in_Flow();
     t+=_dt;
   }
